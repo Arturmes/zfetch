d11c3dabacedc0d3ba6243fc69178e84436e1a59
Arturmes <160504276+Arturmes@users.noreply.github.com>
2024-04-14 07:03:25 +0000

    Small update

--- zfetch.sh
+++ zfetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #    zfetch - a fast but pretty fetch script
-#    Copyright (C) 2022 jornmann
+#    Copyright (C) 2022 - 2024 jornmann, arturmes
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@
 # variables used: $NAME
 # we do a check to see if $NAME is already set, if not, we try to detect OS
 # ourselves
+unset NAME
 if [ "$NAME" = "" ]; then
 	. /etc/os-release 2>/dev/null || export NAME="Unknown"
 fi
@@ -30,31 +31,24 @@ fi
 nc="\033[0m"
 
 # logos
-
 if echo $NAME | grep -q 'Arch'; then
 	dscolor="\033[0;36m" # cyan
-	dslogo1="         "
-	dslogo2="   /\\    "
-	dslogo3="  /' \\   "
-	dslogo4=" /_--_\\  "
-	dslogo5="         "
-	dslogo6="         "
+	dslogo1="        /\         "
+	dslogo2="       /  \        "
+	dslogo3="      /    \       "
+	dslogo4="     /  ,.  \      "
+	dslogo5="    / ,'  ', \     "
+	dslogo6="   /.'      '.\    "
+	dslogo7="                   "
 elif echo $NAME | grep -q 'Artix'; then
 	dscolor="\033[0;36m" # cyan
-	dslogo1="         "
-	dslogo2="   /\\    "
-	dslogo3="  /'-_   "
-	dslogo4=" /_-'_\\  "
-	dslogo5="         "
-	dslogo6="         "
-elif echo $NAME | grep -q 'Zorin'; then
-	dscolor="\033[0;36m" # cyan
-	dslogo1="  ....   "
-	dslogo2=" ...  .  "
-	dslogo3=" .  ...  "
-	dslogo4="  ....   "
-	dslogo5="         "
-	dslogo6="         "
+	dslogo1="        /\         "
+	dslogo2="       /',\        "
+	dslogo3="      /   ,\       "
+	dslogo4="     /  ,'  \      "
+	dslogo5="    / ,'  ', \     "
+	dslogo6="   /.'      '.\    "
+	dslogo7="                   "
 elif echo $NAME | grep -q 'Gentoo'; then
 	dscolor="\033[0;35m" # purple
 	dslogo1="         "
@@ -63,6 +57,7 @@ elif echo $NAME | grep -q 'Gentoo'; then
 	dslogo4="  /__/   "
 	dslogo5="         "
 	dslogo6="         "
+	dslogo7=$dslogo6
 elif echo $NAME | grep -q 'Debian'; then
 	dscolor="\033[0;31m" # red
 	dslogo1="         "
@@ -71,14 +66,7 @@ elif echo $NAME | grep -q 'Debian'; then
 	dslogo4="  '-_    "
 	dslogo5="         "
 	dslogo6="         "
-elif echo $NAME | grep -q 'donut'; then
-	dscolor="\033[0;36m" # red
-	dslogo1="   \$\$\$    "
-	dslogo2=" \$##=##\$  "
-	dslogo3="!!:. .:!! "
-	dslogo4=" \$##@##\$  "
-	dslogo5="   \$\$\$    "
-	dslogo6="          "
+	dslogo7=$dslogo6
 elif echo $NAME | grep -q 'openSUSE'; then
 	dscolor="\033[0;32m" # green
 	dslogo1="         "
@@ -87,6 +75,7 @@ elif echo $NAME | grep -q 'openSUSE'; then
 	dslogo4="  @' '   "
 	dslogo5="         "
 	dslogo6="         "
+	dslogo7=$dslogo6
 elif echo $NAME | grep -q 'Fedora'; then
 	dscolor="\033[0;34m" # blue
 	dslogo1="         "
@@ -95,6 +84,7 @@ elif echo $NAME | grep -q 'Fedora'; then
 	dslogo4=" '-..-'  "
 	dslogo5=" (__/    "
 	dslogo6="         "
+	dslogo7=$dslogo6
 elif echo $NAME | grep -q 'Mint'; then
 	dscolor="\033[0;32m" # blue
 	dslogo1="         "
@@ -103,14 +93,7 @@ elif echo $NAME | grep -q 'Mint'; then
 	dslogo4=" \\____/  "
 	dslogo5="         "
 	dslogo6="         "
-elif echo $NAME | grep -q 'MX'; then
-	dscolor="\033[0;37m" # white
-	dslogo1="         "
-	dslogo2="   \\/    "
-	dslogo3="   /\\    "
-	dslogo4="  /\\ \\   "
-	dslogo5=" /__\\/\\  "
-	dslogo6="         "
+	dslogo7=$dslogo6
 else
 	dscolor="\033[0;37m" # white
 	dslogo1="         "
@@ -119,6 +102,7 @@ else
 	dslogo4="  (^(^)  "
 	dslogo5="         "
 	dslogo6="         "
+	dslogo7=$dslogo6
 fi
 
 
@@ -131,28 +115,88 @@ fi
 [ -e ~/.zfetchrc ] && . ~/.zfetchrc 2> /dev/null
 
 # command line parameters
-if [ "$arg" = "" ]; then
-	arg=""
-elif [ "$arg" = "nologo" ]; then
-	unset dslogo
-elif [ "$arg" = "nofetch" ]; then
-	printf "${dscolor}${dslogo1}\n${dslogo2}\n${dslogo3}\n${dslogo4}\n${dslogo5}\n${dslogo6}\n${nc}"
-	exit
+# if [ "$arg" = "" ]; then
+# 	arg=""
+# elif [ "$arg" = "nologo" ]; then
+# 	unset dslogo
+# elif [ "$arg" = "nofetch" ]; then
+# 	printf "${dscolor}${dslogo1}\n${dslogo2}\n${dslogo3}\n${dslogo4}\n${dslogo5}\n${dslogo6}\n${nc}"
+# 	exit
+# fi
+
+# package manager
+if echo $NAME | grep -q 'Gentoo'; then
+	export pm=$(echo "$(ls /var/db/pkg/*/*/BUILD_TIME 2>/dev/null | wc -l) (portage)")
+elif echo $NAME | grep -q 'Arch'; then
+	export pm=$(echo "$(pacman -Qq 2>/dev/null | wc -l) (pacman)")
+elif echo $NAME | grep -q 'Artix'; then
+	export pm=$(echo "$(pacman -Qq 2>/dev/null | wc -l) (pacman)")
+elif echo $NAME | grep -q 'openSUSE'; then
+	export pm=$(echo "$(rpm -qa 2>/dev/null | wc -l) (zypper)")
+elif echo $NAME | grep -q 'Fedora'; then
+	export pm=$(echo "$(rpm -qa 2>/dev/null | wc -l) (rpm)")
+elif echo $NAME | grep -q 'Debian'; then
+	export pm=$(echo "$(apt list --installed 2>/dev/null | wc -l) (dpkg)")
+elif echo $NAME | grep -q 'Mint'; then
+	export pm=$(echo "$(apt list --installed 2>/dev/null | wc -l) (apt)")
+fi
+
+# disk model
+if [ -f /sys/block/sda/device/model ]; then
+	export diskc="$(cat /sys/block/sdb/device/model)"
+else
+	export diskc=$(echo Unknown)
+fi
+
+# motherboard name
+hostv="$(cat /sys/class/dmi/id/product_name)"
+if [ -f /sys/class/dmi/id/board_name ]; then
+	export hostp=$(cat /sys/class/dmi/id/board_name)
+else
+	export hostp=$(echo Unknown)
+fi
+
+if [ "$hostv" == "Default string" ]; then
+	export hostv=$(echo Unknown)
+fi
+
+# cpu arch
+arc=$(uname -m)
+if [ "$arc" == x86_64 ]; then
+	export arc=$(echo)
+fi
+
+# initd
+if [ -f /sbin/init ]; then
+	export init="$(realpath /sbin/init | sed "s/\/bin\///" | sed "s/\/sbin\///" | sed "s/\/usr//" | sed "s/\/lib//" | sed "s/\-init//" | sed "s/\/systemd//")"
+else
+	export init=$(echo Unknown)
 fi
 
 # the meat and potatoes, actual fetch
-
 host=$(cat /proc/sys/kernel/hostname)
 kernel=$(sed "s/version // ; s/ (.*//" /proc/version)
-uptime=$(uptime -p 2>/dev/null | sed "s/up //")
-shell=$(printf "$SHELL" | sed "s/\/bin\///" | sed "s/\/usr//")
+uptime=$(uptime -p | sed "s/up //")
+shell="$(printf "$SHELL" | sed "s/\/bin\///" | sed "s/\/usr//")"
+session=$XDG_SESSION_TYPE
+terma="$(readlink /proc/$$/fd/2 | sed "s/\/dev//" | sed "s/\///" | sed "s/\///")"
+cpu="$(grep "model name" /proc/cpuinfo | head -n1 | sed "s/\model name	://" | sed "s/\ //" | sed "s/\ CPU//")"
 
-printf "${dscolor}${dslogo1}$USER@$host\n"
-printf "${dscolor}${dslogo2}OS     ${nc} $NAME\n"
-printf "${dscolor}${dslogo3}Kernel ${nc} $kernel\n"
-printf "${dscolor}${dslogo4}Uptime ${nc} $uptime\n"
-printf "${dscolor}${dslogo5}Shell  ${nc} $shell\n"
+printf "${dscolor}${dslogo7}$USER@$host\n"
+printf "${dscolor}${dslogo7}OS      ${nc} $NAME\n"
+printf "${dscolor}${dslogo7}Kernel  ${nc} $kernel $arc\n"
+printf "${dscolor}${dslogo1}Cpu     ${nc} $cpu\n"
+printf "${dscolor}${dslogo2}Host    ${nc} $hostv $hostp\n"
+printf "${dscolor}${dslogo3}Init    ${nc} $init\n"
+printf "${dscolor}${dslogo4}Uptime  ${nc} $uptime\n"
+printf "${dscolor}${dslogo5}Shell   ${nc} $shell\n"
+printf "${dscolor}${dslogo6}Session ${nc} $session\n"
+printf "${dscolor}${dslogo7}Pkgs    ${nc} $pm\n"
+printf "${dscolor}${dslogo7}Term    ${nc} $terma\n"
+printf "${dscolor}${dslogo7}Disk    ${nc} $diskc\n"
 
 if [ "$colorsoff" != 1 ]; then
-	printf "${dslogo6}\033[0;31m● \033[0;32m● \033[0;33m● \033[0;34m● \033[0;35m● \033[0;36m● \033[0;37m●\033[0m\n"
+	printf "${dslogo7}\033[0;31m● \033[0;32m● \033[0;33m● \033[0;34m● \033[0;35m● \033[0;36m● \033[0;37m●\033[0m\n"
 fi
+
+exit $?
